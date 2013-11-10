a98d6f537aad87e7671893aab25990d50929f4c6
tshirtman <gabriel.pettier@gmail.com>
2013-11-10 15:51:42 +0100

    bump

--- main.py
+++ main.py
@@ -10,26 +10,30 @@ from kivy.uix.bubble import Bubble
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.scatter import Scatter
-from kivy.uix.behaviors import ButtonBehavior
+#from kivy.uix.behaviors import ButtonBehavior
 from kivy.factory import Factory
+from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.lib import osc
 from os import listdir as oslistdir
 from os.path import join, isdir
 import socket
+import os
 
 outSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 SERVER = '192.168.1.10'
 PORT = 3000
 
-PAINTED = 'tapisserie'
+PAINTED = 'image-fond-vignettes'
 POSTER = 'affiche'
 WINDOW = 'vidéo'
 SOUND = 'son'
 OBJECT = 'objet'
 
-#PAINTED = 'images-fond'
-#OBJECT = 'images-objets'
+#PAINTED = 'fond'
+#OBJECT = 'objets'
+
+MINIATURES = '-vignettes'
 
 types = PAINTED, POSTER, WINDOW, SOUND, OBJECT
 
@@ -61,16 +65,31 @@ class Poster(Scatter):
                   scale=self.send_update)
 
     def send_update(self, *args):
-        return
         osc_send('/update',
-                 [self.x, self,y, self.parent.children.index(self),
+                 [self.source,
+                  self.x / self.parent.width,
+                  self.y / self.parent.height,
+                  self.parent.children.index(self),
                   self.scale, self.rotation])
+
+    def on_touch_down(self, touch, *args):
+        if touch.is_double_tap:
+            self.ids.close.disabled = False
+            Animation(opacity=1, d=.1).start(self.ids.close)
+        return super(Poster, self).on_touch_down(touch, *args)
 
     def on_touch_up(self, touch):
         roomview = app.root.ids.roomview
         if self.parent == roomview.ids.dnd:
             roomview.ids.dnd.remove_widget(self)
             roomview.ids.posters.add_widget(self)
+
+        if not  self.ids.close.disabled:
+            a = Animation(d=5)
+            a &= Animation(opacity=0, d=2)
+            a.bind(on_complete=lambda *x:
+                               self.ids.close.setter('disabled')(self.ids.close, True))
+            a.start(self.ids.close)
 
         return super(Poster, self).on_touch_up(touch)
 
@@ -91,14 +110,17 @@ class Choser(BoxLayout):
             choices = app.root.ids.choices
             choices.clear_widgets()
             for f in app.choices.get(self.choice, []):
+                if isdir(f):
+                    continue
                 choices.add_widget(
                     Factory.Choice(source=join('data', 'resources', self.choice, f)))
 
 
-class Choice(Image, ButtonBehavior):
+class Choice(Image):
     def manage(self, touch, *args):
-        if self.collide_point(*touch.pos) or touch.grab_current == self:
+        if self.collide_point(*touch.pos):
            print "manage"
+           #app.manage_choice(touch, self.source.replace(MINIATURES, ''))
            app.manage_choice(touch, self.source)
 
     def on_touch_down(self, touch, *args):
@@ -129,21 +151,26 @@ class PimpApp(App):
         self.choices = choices
 
     def on_wallpaper(self, *args):
-        return
-        osc_send('/wallpaper', self.wallpaper)
+        print "sending wallpaper"
+        osc_send('/wallpaper', [self.wallpaper.split(os.sep)[-1], ])
 
     def manage_choice(self, touch, choice):
         if istype(choice, PAINTED):
-            self.wallpaper = choice
+            self.wallpaper = choice.replace(MINIATURES, '')
 
         elif istype(choice, POSTER):
             if 'poster' not in touch.ud:
                 poster = Factory.Poster(
+                        scale=0.1,
                         source=choice,
                         center=touch.pos,
                         size_hint=(None, None))
                 self.root.ids.roomview.ids.dnd.add_widget(poster)
                 touch.ud['poster'] = poster
+                widgets = [x for x in app.root.ids.choices.children if x.source == choice]
+                if widgets:
+                    widgets[0].disabled = True
+
                 poster.on_touch_down(touch)
             else:
                 touch.ud['poster'].center = touch.pos
@@ -157,7 +184,10 @@ class PimpApp(App):
             self.ambiant_sound = None
         print self.ambiant
         self.ambiant_sound = SoundLoader.load(self.ambiant)
-        self.ambiant_sound.state = 'play'
+        if self.ambiant_sound:
+            self.ambiant_sound.state = 'play'
+        else:
+            print 'error loading %s' % self.ambiant
 
 
 if __name__ == '__main__':
